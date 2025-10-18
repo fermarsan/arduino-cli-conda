f286225c25fdc47e728c77230824e6f9cfef2d88
fermarsan <fmartinezsanta@gmail.com>
2025-10-18 18:25:28 -0500

    update

--- bld.bat
+++ bld.bat
@@ -2,9 +2,6 @@
 
 :: Copy the arduino-cli executable to the bin directory
 mkdir %PREFIX%\bin
-copy arduino-cli.exe %PREFIX%\bin\
+copy %SRC_DIR%\win-64\bin\arduino-cli.exe %PREFIX%\bin\
 if errorlevel 1 exit 1
 
-:: Copy the license file
-copy LICENSE.txt %PREFIX%\bin\
-if errorlevel 1 exit 1
